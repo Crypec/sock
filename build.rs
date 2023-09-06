844f73638f17c9c9f68406b37448c473ede07fe1
.xtc <ctx@systemli.org>
2023-09-06 12:53:02 +0200

    remove tdoku submodule

--- build.rs
+++ build.rs
@@ -1,5 +1,9 @@
+use std::process::Command;
+
 fn main() {
     println!("cargo:rerun-if-changed=./test_data/");
+
+    // build tdudoku solver
 
     let test_files_path = std::fs::read_dir("./test_data/").expect("failed to find test_data path");
     for path in test_files_path {
@@ -8,7 +12,7 @@ fn main() {
         println!("{file_stem:?}");
         // println!("Name: {}", path.unwrap().path().display())
     }
-    panic!("stop");
+    // panic!("stop");
 }
 
 fn write_test(path: String, name: &str, file: std::fs::File) {}

0d52cf8596290414f2c1f882f3075d467bf76ba0
.xtc <ctx@systemli.org>
2023-09-06 19:59:35 +0200

    change cargo project to lib instead of binary package

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![feature(let_chains)]
+#![warn(clippy::nursery)]
+#![warn(clippy::pedantic)]
+#![allow(internal_features)]
+#![feature(rustc_attrs)]
+#![feature(associated_type_bounds)]
+#![feature(assert_matches)]
+#![feature(test)]
+#![allow(dead_code)]
+
+pub mod board;
+pub mod solver;
+mod visualize;
